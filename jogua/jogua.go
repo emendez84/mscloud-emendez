41e97f3da7e162f1d5bac716a6fd29a42576d4e7
Pedro Flores <pflores@dncp.gov.py>
2014-07-01 22:16:37 -0400

    Sell and Buy modules added

--- jogua/jogua.go
+++ jogua/jogua.go
@@ -3,12 +3,50 @@ package jogua
 import (
 	"fmt"
 	"net/http"
+	"appengine"
+	"appengine/datastore"
+	"strconv"
 )
+
+type Stock struct {
+	Empresa  string
+	Puntos int
+}
  
 func init() {
 	http.HandleFunc("/", handleStart)
 }
  
 func handleStart(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "jogua.handleStart")
+	c := appengine.NewContext(r)
+
+	// Get the key from the URL
+	keyField := r.FormValue("key")
+
+	// Decode the key
+	key, err := datastore.DecodeKey(keyField)
+	if err != nil { // Couldn't decode the key
+		// Do some error handling
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Get the key and load it into "data"
+	var data Stock
+	err = datastore.Get(c, key, &data)
+	if err != nil { // Couldn't find the entity
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	sellField, err := strconv.Atoi(r.FormValue("sell"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	data.Puntos = data.Puntos + sellField
+
+	datastore.Put(c, key, &data)
+
+	fmt.Fprintln(w, "Empresa: ", data.Empresa, "Puntos: ", data.Puntos)
 }
